1e29b02a11e5c4dc11a8dec6da8f24f81f353213
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-11-03 15:39:10 -0600

    first

---
+++ Translator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Translator{
+
+public static void main (String[] args){
+Scanner scnr = new Scanner(System.in);
+
+System.out.println("enter a string please");
+String userInput = scnr.nextLine();
+
+
+
+}
+
+
+
+}
---
+++ concatenator.java
@@ -0,0 +1,10 @@
+public class concatenator{
+
+    public static void concat(String s) {
+        str [] words = s.split(" ");
+for (int i = 0; i < words.length; i++){
+    System.out.println(words[i] + " psss" );
+}
+
+    }
+}
